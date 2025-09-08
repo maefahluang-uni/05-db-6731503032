11639ed8640ba18335d1ea18c0e00ea64905c84b
6731503032 <6731503032@lamduan.mfu.ac.th>
2025-09-08 09:42:29 +0000

    test

--- src/main/java/th/mfu/boot/App.java
+++ src/main/java/th/mfu/boot/App.java
@@ -2,7 +2,7 @@ package th.mfu.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//commit test
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
